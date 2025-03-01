5970bf2d63c05f20ab783c8ed837c0c3403c18c2
vermaAtul1520 <atulverma1520@gmail.com>
2025-03-01 13:59:22 +0530

    url change of resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -48,8 +48,7 @@ const greeting = {
     "- Social Media Marketing\n" +
     "- Influence Marketing"
   ),
-  resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Same resume link
+  resumeLink: "https://drive.google.com/file/d/143AxkNoiewGZ7WvF5oxqcLM2-AZP9Uvc/view?usp=sharing", // Same resume link
   displayGreeting: true // Set false to hide this section
 };
 
@@ -58,13 +57,11 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/vermaAtul1520",
+  linkedin: "https://www.linkedin.com/in/atul-kumar-verma-1514121a3/",
+  gmail: "atulverma1520@gmail.com",
+  // facebook: "https://www.facebook.com/saad.pasta7",
+  // stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
